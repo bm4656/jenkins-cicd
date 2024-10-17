520b087220704a5d2810224b60a9458504a3eab5
bm4656 <newjin46@gmail.com>
2024-10-17 18:01:38 +0900

    feat: 도커 설치된 컨테이너 구동

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello jenkins!';
+    return 'Hello jenkins! with container';
   }
 }
